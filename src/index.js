3da525689a768a3dd3ccc69387aa03b51866033b
Ana Graca <Ana.Graca@kyndryl.com>
2023-06-02 15:17:06 +0100

    Formulários Produtos

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
-import Produtos from './pages/Produtos';
+import Produtos from './pages/Produtos/Produtos';
+import CriarProduto from './pages/Produtos/CriarProduto';
+import EditarProduto from './pages/Produtos/EditarProduto';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,6 +18,8 @@ root.render(
           
           <Route path="/home" element={<Home />} />
           <Route path="/produtos" element={<Produtos />} />
+          <Route path="/criar-produto" element={<CriarProduto />} />
+          <Route path="/edtar-produto" element={<EditarProduto />} />
           {/* <Route path="utilizadores" element={<Utilizadores />} />
           <Route path="*" element={<NoPage />} /> */}
       </Routes>
---
+++ src/pages/Produtos/Produtos.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+import { Link } from 'react-router-dom';
+import Layout from '../../components/Layout';
+import Button from 'react-bootstrap/Button';
+import EditarProduto from './EditarProduto';
+
+const labels = ['Nome', 'Preço', 'Descrição', 'Tamanho', 'Género', 'Cor', 'Coleção', 'Categoria', 'Marca', 'Utilizador']
+const produtos = [
+  {
+    id: 1,
+    nome: 'Produto 1',
+    preco: '10,99 €',
+    descricao: 'Descrição 1',
+    tamanho: 'XL',
+    genero: 'Masculino',
+    cor: 'Vermelho',
+    colecao: 'Coleção 1',
+    categoria: 'Category 1',
+    marca: 'Marca 1',
+    utilizador: 'Utilizador 1',
+  },
+  {
+    id: 2,
+    nome: 'Produto 2',
+    preco: '19,99 €',
+    descricao: 'Descrição 2',
+    tamanho: 'L',
+    genero: 'Feminino',
+    cor: 'Azul',
+    colecao: 'Coleção 2',
+    categoria: 'Category 2',
+    marca: 'Marca 2',
+    utilizador: 'Utilizador 2',
+  },
+  {
+    id: 3,
+    nome: 'Produto 3',
+    preco: '14,99 €',
+    descricao: 'Descrição 3',
+    tamanho: 'M',
+    genero: 'Masculino',
+    cor: 'Verde',
+    colecao: 'Coleção 3',
+    categoria: 'Category 3',
+    marca: 'Marca 3',
+    utilizador: 'Utilizador 3',
+  },
+]
+
+function Produtos() {
+
+  const handleEditarProduto = (produto) => {
+    <EditarProduto product={produto.id} />
+  };
+
+  return (
+    <div>
+      <Layout />
+      <div className="container">
+        <h1>Lista de Produtos</h1>
+        <Link to="/criar-produto">
+          <Button variant="primary">Criar novo Produto</Button>
+        </Link>
+        <Table responsive>
+          <thead className="thead-dark">
+          <tr>
+              {labels.map((label) => (
+                <th key={label}>{label}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {produtos.map((produto) => (
+              <tr key={produto.id}>
+                <td>{produto.nome}</td>
+                <td>{produto.preco}</td>
+                <td>{produto.descricao}</td>
+                <td>{produto.tamanho}</td>
+                <td>{produto.genero}</td>
+                <td>{produto.cor}</td>
+                <td>{produto.colecao}</td>
+                <td>{produto.categoria}</td>
+                <td>{produto.marca}</td>
+                <td>{produto.utilizador}</td>
+                <td>
+                <a href='#' onClick={() => handleEditarProduto(produto)}> Editar </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  );
+}
+
+export default Produtos;
